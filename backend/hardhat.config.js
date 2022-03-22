ec87a0567c7301f2920278a996634367d88285dd
Tomiwa Adeyemi <adeyemitomiwa7@gmail.com>
2022-03-22 12:38:27 +0100

    Add files via upload

---
+++ backend/hardhat.config.js
@@ -0,0 +1,19 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.2",
+   defaultNetwork: "rinkeby",
+   networks: {
+      hardhat: {},
+      rinkeby: {
+         url: API_URL,
+         accounts: [PRIVATE_KEY]
+      }
+   },
+}
