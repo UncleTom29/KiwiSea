3235f00309794497c4992ca3875abf57010557de
Tomiwa Adeyemi <adeyemitomiwa7@gmail.com>
2022-03-22 12:15:33 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,153 @@
+import "./App.css";
+import { Contract, ethers } from "ethers";
+import { useEffect, useState } from "react";
+import contractABI from "./contractABI.json";
+
+const contractAddress = 0x024527B485Bb7b0cdf378E4011Eb892840895345;
+
+function App() {
+ 
+	const [account, setAccount] = useState(null);
+	const [isWalletInstalled, setIsWalletInstalled] = useState(false);
+  const [NFTContract, setNFTContract] = useState(null);
+  // state for whether app is minting or not.
+	const [isMinting, setIsMinting] = useState(false);
+
+
+  useEffect(() => {
+		if (window.ethereum) {
+			setIsWalletInstalled(true);
+		}
+	}, []);
+
+  useEffect(() => {
+		function initNFTContract() {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      setNFTContract(new Contract(contractAddress,contractABI.abi,signer));
+		}
+		initNFTContract();
+	}, [account]);
+
+
+  async function connectWallet() {
+		window.ethereum
+			.request({
+				method: "eth_requestAccounts",
+			})
+			.then((accounts) => {
+				setAccount(accounts[0]);
+			})
+			.catch((error) => {
+				alert("Something went wrong");
+			});
+	}
+ 
+
+    const data = [
+        {
+            url: "./assets/images/1.png",
+            param: "handleMint('<your-metadata-pinata-link-here>')",
+        },
+        {
+          url: "./assets/images/2.png",
+            param: "handleMint('<your-metadata-pinata-link-here>')",
+        },
+        {
+          url: "./assets/images/3.png",
+            param: "handleMint('<your-metadata-pinata-link-here>')",
+        },
+        {
+          url: "./assets/images/4.png",
+            param: "handleMint('<your-metadata-pinata-link-here>')",
+        },
+        {
+          url: "./assets/images/5.png",
+            param: "handleMint('<your-metadata-pinata-link-here>')",
+        },
+    ];
+
+    async function withdrawMoney(){
+        try {
+ 
+            const response = await NFTContract.withdrawMoney();
+            console.log("Received: ", response);
+          } catch (err) {
+              alert(err);
+          }
+          
+    }
+//eslint ignore
+    async function handleMint(tokenURI) {
+        setIsMinting(true);
+            try {
+              const options = {value: ethers.utils.parseEther("0.01")};
+              const response = await NFTContract.mintNFT(tokenURI, options);
+              console.log("Received: ", response);
+            } catch (err) {
+                alert(err);
+            }
+            finally {
+              setIsMinting(false);
+            }
+    }
+
+    if (account === null) {
+      return (
+        <>
+         <div className="container">
+           <br/>
+          <h1> 🔮 KiwiSea </h1>
+          <h2>NFT Marketplace</h2>
+          <p>Buy an NFT from our marketplace.</p>
+  
+          {isWalletInstalled ? (
+            <button onClick={connectWallet}>Connect Wallet</button>
+          ) : (
+            <p>Install Metamask wallet</p>
+          )}
+          </div>
+          </>
+      );
+    }
+
+    return (
+        <>
+            <div className="container">
+            <br/>
+            <h1> 🔮 KiwiSea </h1>
+          
+             <h2>NFT Marketplace</h2>
+                {data.map((item, index) => (
+                    <div className="imgDiv">
+                        <img
+                            src={item.url}
+                            key={index}
+                            alt="images"
+                            width={250}
+                            height={250}
+                        />
+                        <button isLoading={isMinting}
+                            onClick={() => {
+                                eval(item.param);
+                            }}
+                        >
+                            Mint - 0.01 eth
+                        </button>
+                    </div>
+                ))}
+                 <button 
+                            onClick={() => {
+                                withdrawMoney();
+                            }}
+                        >
+                            Withdraw Money from Contract
+                 </button>
+          
+        </div>
+
+        </>
+    );
+}
+
+export default App;
